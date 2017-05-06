0a262db42df611ae625fd7fafd221ae1131a43d1
Rich Greenhill <rgnet1@yahoo.com>
2017-05-06 00:24:39 -0700

    ball moves

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "indent": [2, 4],
+        "jsx-a11y/img-has-alt": 0,
+        "react/jsx-space-before-closing": 0,
+        "react/jsx-indent": [2, 4],
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": 1,
+    }
+};
---
+++ main.js
@@ -0,0 +1,24 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Ball from './src/Ball';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
+
+class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Ball />
+            </View>
+        );
+    }
+}
+
+Expo.registerRootComponent(App);
