adde6c16b6bcbcc577700a59831af9658b9972d6
Rich Greenhill <rgnet1@yahoo.com>
2017-05-07 13:46:22 -0700

    add single card animate and rotate

--- src/Deck.js
+++ src/Deck.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import {
     View,
     Animated,
-    Text,
+    PanResponder,
 } from 'react-native';
 
 const propTypes = {
@@ -13,15 +13,57 @@ const propTypes = {
 };
 
 class Deck extends Component {
+    constructor(props) {
+        super(props);
+
+        this.position = new Animated.ValueXY();
+
+        this.panResponder = PanResponder.create({
+            // (return bool) if user presses on screen, be responsible for it
+            onStartShouldSetPanResponder: () => true,
+            // (cb) called any time user starts dragging around screen
+            onPanResponderMove: (_evt, gesture) => {
+                this.position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            // (cb) called when finger lets go after dragging
+            onPanResponderRelease: () => {},
+        });
+    }
+
+    getCardStyle() {
+        const rotate = this.position.x.interpolate({
+            inputRange: [-500, 0, 500],
+            outputRange: ['-120deg', '0deg', '120deg'],
+        });
+
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate }],
+        };
+    }
+
     renderCards() {
-        return this.props.data.map(item => this.props.renderCard(item));
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
+            return this.props.renderCard(item);
+        });
     }
 
     render() {
         const { renderNoMoreCards, data, onSwipeRight, onSwipeLeft } = this.props;
         return (
             <View>
-                {this.renderCards()}        
+                {this.renderCards()}
             </View>
         );
     }
